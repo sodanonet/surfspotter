564f407c6195f26976bfdd591e62b5ea0378d90f
sodanonet <sodano.me@gmail.com>
2025-10-21 20:26:17 +0200

    add spot details initial react setup with vite and typescript

---
+++ spot-details/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Spot Details</h1>
+      <p>React microfrontend for displaying surf spot information</p>
+    </div>
+  )
+}
+
+export default App
